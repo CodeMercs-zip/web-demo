6ed4c9ec3fb186fd1a34a8835fdb7c0834e5fe71
scv718 <skfnxh718@naver.com>
2025-07-24 01:59:13 +0900

    no message

--- src/main/java/com/rgs/web_demo/controller/MainController.java
+++ src/main/java/com/rgs/web_demo/controller/MainController.java
@@ -1,12 +1,12 @@
 package com.rgs.web_demo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
 
-    @GetMapping("/hello")
+    @PostMapping("/hello")
     public String hello() {
         return "Hello, World!";
     }
