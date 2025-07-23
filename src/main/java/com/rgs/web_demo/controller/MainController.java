2714c53e4fbc32a37e85bc47ceb58c9aa9121e38
scv718 <skfnxh718@naver.com>
2025-07-24 03:42:51 +0900

    no message

--- src/main/java/com/rgs/web_demo/controller/MainController.java
+++ src/main/java/com/rgs/web_demo/controller/MainController.java
@@ -8,7 +8,7 @@ public class MainController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello, W orld!";
+        return "Hello, World!";
     }
 
     @GetMapping("/hello3")
