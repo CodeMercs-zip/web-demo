04363d81f86ca6475c0185a120303d57f1540eda
scv718 <skfnxh718@naver.com>
2025-08-04 23:15:41 +0900

    MyBatisConfig 주석 해제

--- src/main/java/com/rgs/web_demo/mapper/MemberMapper.java
+++ src/main/java/com/rgs/web_demo/mapper/MemberMapper.java
@@ -1,15 +1,15 @@
- package com.rgs.web_demo.mapper;
+package com.rgs.web_demo.mapper;
 
- import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Mapper;
 
- import com.rgs.web_demo.vo.MemberVo;
+import com.rgs.web_demo.vo.MemberVo;
 
- @Mapper
- public interface MemberMapper {
+@Mapper
+public interface MemberMapper {
 
-     // 이메일로 회원 1명 조회
-     MemberVo selectMemberByEmail(String email);
+	// 이메일로 회원 1명 조회
+	MemberVo selectMemberByEmail(String email);
 
-     // 회원 등록
-     int insertMember(MemberVo member);
- }
+	// 회원 등록
+	int insertMember(MemberVo member);
+}
